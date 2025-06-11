c074a786597fe1ab83b490f7dd6ff585d855d18b
Pedro Arruda Alves <24011669@fecap.br>
2025-06-11 12:05:11 -0300

    texto

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return render_template('index.html')
+
+@app.route('/pag1')
+def home():
+    return render_template('pag1.html')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
